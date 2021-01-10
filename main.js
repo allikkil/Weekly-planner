7fcd0a3da22950ce4d7e881fac58ad9dd4b88aba
Killu Allik <killuallik@gmail.com>
2021-01-10 19:23:01 +0200

    Tasks are now saved locally

--- main.js
+++ main.js
@@ -1,57 +1,88 @@
-const allTasks = document.querySelectorAll('.new-task');
+const newTaskContainers = document.querySelectorAll('.new-task');
 const taskList = document.querySelectorAll('.task-list');
 const taskTemplate = document.querySelector('#task-template');
 
+// Update day and weekday on load
 
-let tasks = [
-    {
-        id: 1,
-        input: allTasks[0],
-        list: taskList[0]
-    },
-    {
-        id: 1,
-        input: allTasks[1],
-        list: taskList[1]
-    },
-    {
-        id: 1,
-        input: allTasks[2],
-        list: taskList[2]
-    },
-    {
-        id: 1,
-        input: allTasks[3],
-        list: taskList[3]
-    },
-    {
-        id: 1,
-        input: allTasks[4],
-        list: taskList[4]
-    }
-]
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const weekdayContainer = document.getElementsByClassName("weekday");
+const dayContainer = document.getElementsByClassName("day");
 
-tasks.forEach(task => task.input.addEventListener('keyup', (e) => {
+let tasks = []
 
-    if (e.keyCode === 13 && validInput(task.input.value)) {
-        addTask(task)
-    }
-}))
+var dateCurrent = new Date();
 
+for (let i = 0; i < 5; i++) {
 
-function addTask(x) {
-    const taskElement = document.importNode(taskTemplate.content, true);
-    const checkbox = taskElement.querySelector('input');
-    checkbox.id = x.id;
-    const label = taskElement.querySelector('label');
-    label.htmlFor = x.id;
-    label.append(x.input.value);
-    x.list.appendChild(taskElement);
-    x.input.value = '';
-    x.id++;
+    var day = 60 * 60 * 24 * i * 1000;
+
+    var endDate = new Date(dateCurrent.getTime() + day);
+    var DOW = weekday[endDate.getDay()];
+
+    weekdayContainer[i].innerHTML = DOW;
+    dayContainer[i].innerHTML = endDate.getDate();
+    var dateFull = endDate.getDate() + " " + month[endDate.getMonth()] + " " + endDate.getFullYear();
+
+    tasks.push({
+        date: dateFull,
+        input: newTaskContainers[i],
+        list: taskList[i]
+
+    });
 
 }
 
+window.addEventListener('load', (e) => {
+    tasks.forEach(task => {
+        if (localStorage.getItem(task.date) !== "") {
+            var loadList = localStorage.getItem(task.date);
+            loadList = loadList.split(',');
+
+            loadList.forEach(item => {
+                task.input.value = item;
+                addTask(task);
+            });
+        }
+
+
+
+
+    });
+});
+tasks.forEach(task => task.input.addEventListener('keyup', (e) => {
+
+    if (e.keyCode === 13 && validInput(task.input.value)) {
+        addTask(task);
+        saveList();
+    }
+}))
+
+// Saves date and list as a key-value pair
+function saveList() {
+    for (let index = 0; index < 5; index++) {
+        let task = tasks[index];
+        dateFull = task.date;
+
+        let labels = [];
+
+        task.list.querySelectorAll(".task").forEach(item => labels.push(item.childNodes[3].innerHTML));
+        localStorage.setItem(dateFull, labels);
+
+    }
+}
+
+function addTask(x) {
+
+    const taskElement = document.importNode(taskTemplate.content, true);
+    const label = taskElement.querySelector('label');
+    label.append(x.input.value);
+    x.list.appendChild(taskElement);
+    x.input.value = '';
+
+}
+
+// Checks if input is empty/ consists of spaces
 function validInput(input) {
     input = input.replace(/\s+/g, '');
     if (input !== '') {
@@ -60,28 +91,10 @@ function validInput(input) {
     return false;
 }
 
+// When task is completed, it's automatically removed
 document.body.addEventListener('click', (e) => {
-    if(e.target.tagName === 'LABEL' || e.target.getAttribute('type') === "checkbox"){
+    if (e.target.tagName === 'LABEL' || e.target.getAttribute('type') === "checkbox") {
         e.target.parentNode.remove();
+        saveList();
     }
 });
-
-// Update day and weekday on load
-
-const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-const weekdayContainer = document.getElementsByClassName("weekday");
-const dayContainer = document.getElementsByClassName("day");
-
-
-var dateCurrent = new Date();
-for (let i = 0; i < 5; i++) {
-
-    var day = 60 * 60 * 24 * i * 1000
-
-    var endDate = new Date(dateCurrent.getTime() + day)
-    var DOW = weekday[endDate.getDay()]
-
-    weekdayContainer[i].innerHTML = DOW
-    dayContainer[i].innerHTML = endDate.getDate()
-
-}
